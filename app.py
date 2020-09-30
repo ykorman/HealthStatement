903174dbfde3bf7719e2e95c017c784d5800d43a
Yigal Korman <ykorman@gmail.com>
2020-09-30 22:06:30 +0300

    basic working version
    Signed-off-by: Yigal Korman <ykorman@gmail.com>

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, send_file
+from pdfmerge import gen_filled_form
+import os
 
 app = Flask(__name__)
 
 @app.route('/',methods=('GET', 'POST'))
-def main():
+def index():
     if request.method == 'POST':
-        print('got:' + request.form['child_name'])
+        gen_filled_form(request.form)
+        return redirect(url_for('download'))
+
     return render_template('index.html')
+
+@app.route('/download')
+def download():
+    return send_file('merged_form.pdf')
