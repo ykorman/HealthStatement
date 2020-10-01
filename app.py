24457cc9d6f6fe92d1426d2024b61b4602eaa5c9
Yigal Korman <ykorman@gmail.com>
2020-10-01 14:39:24 +0300

    index: always fill current date
    Signed-off-by: Yigal Korman <ykorman@gmail.com>

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, send_file, make_response
 from pdfmerge import gen_filled_form
 import os
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -12,7 +13,7 @@ def index():
         list(map(lambda e: resp.set_cookie(e[0], e[1]), request.form.items()))
         return resp
 
-    return render_template('index.html')
+    return render_template('index.html', cur_date=datetime.now().strftime('%d/%m/%Y'))
 
 @app.route('/download')
 def download():
