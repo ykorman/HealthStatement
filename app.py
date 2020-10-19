99a89250727157330b288a99cabc4fd59f4acfef
Yigal Korman <ykorman@gmail.com>
2020-10-19 12:15:33 +0300

    worker and app not on same dyno
    So must transfter attachment file some other way.
    Choose to save it into redis, but in the future, might just generate it
    on the worker again.
    
    Signed-off-by: Yigal Korman <ykorman@gmail.com>

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for, send_file, make_response
 from celery import Celery
-import os
 from datetime import datetime
 from pdfmerge import gen_filled_form
 from send_mail import send_mail
+import redis
+import base64
 
 app = Flask(__name__)
 celery = Celery(app.name, broker=os.environ.get('REDIS_URL'))
@@ -12,9 +14,14 @@ def get_form_name(form_id):
     return 'health_statement_' + form_id + '_' + datetime.now().strftime('%d_%m_%Y' + '.pdf')
 
 @celery.task
-def send_background_mail(form_name, creds, msg):
+def send_background_mail(creds, msg):
+    r = redis.from_url(os.environ.get('REDIS_URL'))
+    form_data = r.get(msg['attachment'])
+    with open(msg['attachment'], 'wb') as f:
+        f.write(base64.b64decode(form_data))
+    r.delete(msg['attachment'])
     send_mail(creds, msg)
-    os.unlink(form_name)
+    os.unlink(msg['attachment'])
 
 @app.route('/',methods=('GET', 'POST'))
 def index():
@@ -28,7 +35,7 @@ def index():
 
 @app.route('/download')
 def download():
-    form_id = request.cookies.get('child_id')
+    form_name = get_form_name(request.cookies.get('child_id'))
     creds = dict()
     creds['smtp'] = os.environ.get('MAIL_SMTP')
     creds['username'] = os.environ.get('MAIL_USER')
@@ -36,8 +43,12 @@ def download():
     msg = dict()
     msg['subject'] = u'הצהרת בריאות הילד לתאריך' + ' ' + datetime.now().strftime('%d/%m/%Y')
     msg['from'] = creds['username']
-    msg['attachment'] = get_form_name(form_id)
+    msg['attachment'] = form_name
     msg['destination'] = request.cookies.get('parent_email')
     msg['body'] = u'מצורפת הצהרת בריאות חתומה'
-    send_background_mail.apply_async(args=(get_form_name(form_id), creds, msg))
-    return send_file(get_form_name(form_id))
+    with open(form_name, 'rb') as f:
+        form_data = base64.b64encode(f.read())
+    r = redis.from_url(os.environ.get('REDIS_URL'))
+    r.set(form_name, form_data)
+    send_background_mail.apply_async(args=(creds, msg))
+    return send_file(form_name)
